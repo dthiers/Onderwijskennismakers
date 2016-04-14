45c568203875d6f049676912e7023bce39390f34
tjvanos <tjos@student.avans.nl>
2016-04-14 17:18:49 +0200

    sherm links plus zoek start

---
+++ js/Dummy/data.js
@@ -0,0 +1,20 @@
+var dummy_data = {
+    schools: [
+        { name: 'de Viersprong',  tags: [{ name: "school" }], experts:"4", bronnen:"18"},
+        { name: 'de Klimboom',  tags: [{ name: "ict" }], experts:"3", bronnen:"11" },
+        { name: 'BBS Bosveld',  tags: [{ name: "bijles" }], experts:"4", bronnen:"22" },
+        { name: 'de Kameleon', tags: [{ name: "onderwijs" }], experts:"2", bronnen:"6" }
+    ],
+    persons: [
+        { name: 'Kristian van den Berg', school: "de Viersprong", tags: [{ name: "school" }], text: "Houdt van school" },
+        { name: 'Jan Timmers', school: "de Viersprong", tags: [{ name: "ict" }], text: "Houdt van ict gerlateerde hulpmiddelen" },
+        { name: 'Angela Horsten', school: "de Viersprong", tags: [{ name: "bijles" }], text: "Houdt van leerlingen ondersteunen" },
+        { name: 'Tom Oosterhuis', school: "de Viersprong", tags: [{ name: "onderwijs" }], text: "Houdt van onderwijs" }
+    ],
+    search: [
+        { name: 'term 1 School', tags: [{ name: "school" }, { name: "onderwijs" }], text: "asfdgsdfgsdfg" },
+        { name: 'term 2 E-learning', tags: [{ name: "ict" }, { name: "onderwijs" }], text: "asfdgsdfgsdfg" },
+        { name: 'term 3 Ondersteuning', tags: [{ name: "bijles" }, { name: "onderwijs" }], text: "asfdgsdfgsdfg" },
+        { name: 'term 4 tablet learning', tags: [{ name: "ict" }, { name: "onderwijs" }], text: "asfdgsdfgsdfg" }
+    ]
+};
