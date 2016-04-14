c7523bc5af044978c400edfc954b7952ce1dcef2
Tijn Kanters <trkanter@avans.nl>
2016-04-14 17:49:47 +0200

    Light school overview fix

--- js/controllers/schoolProfileCtrl.js
+++ js/controllers/schoolProfileCtrl.js
@@ -2,7 +2,7 @@ app.controller('schoolProfileCtrl', ['$scope', '$routeParams', function(sc, rout
 
   sc.$on('$viewContentLoaded', function(event) {
     	for(var i = 0; i < 90; i++){
-		$('#content-box').append('<div class="content-container color2 darken"><div class="content-header"><h4>ICT In Bedrijf</h4><img src="images/document.png"></div><div class="content-author"><p>Auteur: Patrick Duffy</p><p>Datum: 29/03/2016</p></div><div class="content-rating"><img src="images/star-filled.png" width="25" height="25"><img src="images/star-filled.png" width="25" height="25"><img src="images/star-filled.png" width="25" height="25"><img src="images/star-half-filled.png" width="25" height="25"><img src="images/star-empty.png" width="25" height="25"><p>Beoordeling</p></div></div>');
+		$('#content-box').append('<div class="content-container color2 darken"><div class="content-header"><h4>ICT In Bedrijf</h4><img src="images/document.png"></div><div class="content-author"><p>Beschrijving: Dit is de beschrijving van het artikel ICT in bedrijf</p><p>Datum: 29/03/2016</p></div><div class="content-author"><p>Auteur: Patrick Duffy</p><p>Datum: 29/03/2016</p></div><div class="content-rating"><img src="images/star-filled.png" width="25" height="25"><img src="images/star-filled.png" width="25" height="25"><img src="images/star-filled.png" width="25" height="25"><img src="images/star-half-filled.png" width="25" height="25"><img src="images/star-empty.png" width="25" height="25"><p>Beoordeling</p></div></div>');
 	}
   });
 }]);
