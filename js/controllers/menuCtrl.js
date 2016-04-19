0b45185dcb548014af4ca66bee712b398fb24b47
Kai <kaiherwijn@gmail.com>
2016-04-19 17:08:40 +0200

    items klikbaar gemaakt

--- js/controllers/menuCtrl.js
+++ js/controllers/menuCtrl.js
@@ -1,9 +1,9 @@
 app.controller('menuCtrl', ['$scope', '$routeParams', function(sc, routeParams){
   sc.items = [
-        {"title":"Zoeken"},
-        {"title":"School toevoegen"},
-        {"title":"Content aanmaken"},
-        {"title":"Contact opnemen"},
-        {"title":"Mijn updates"}
+        {"title":"Zoeken", "url":"search"},
+        {"title":"School toevoegen", "url":""},
+        {"title":"Content aanmaken", "url":""},
+        {"title":"Contact opnemen", "url":""},
+        {"title":"Mijn updates", "url":""}
     ];
 }]);
