4791cc71f8a2478e0e0c2764403f1cb9637ac304
tjvanos <tjos@student.avans.nl>
2016-04-25 17:05:17 +0200

    first version back to front end
    user from the database to the view

--- app/js/controllers/dashboardCtrl.js
+++ app/js/controllers/dashboardCtrl.js
@@ -1,4 +1,20 @@
-module.exports = function ($scope, VisDataSet) {
+module.exports = function ($scope, $stateParams, VisDataSet, ProfileService) {
+    
+    //geting the user using state params
+    getUser();
+
+    function getUser() {//based on state params
+        ProfileService.getUser(2)//call to service hard coded user 2 = TJ van Os TODO: $stateParams.id
+            .then(function (response) {
+                $scope.user=response.data.data[0];//set response to scope
+                console.dir($scope);
+            }, function (error) {
+                $scope.status = 'Unable to load customer data: ' + error.message;
+            });
+    }
+    
+    
+    
     $scope.slideDown = function(){
         $scope.topBarStyle = {top: '100%'};
         $scope.topContentStyle = {top: '0%'};
