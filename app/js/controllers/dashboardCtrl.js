f83878d3c4c041740f0b879dc40bb07cdaedd414
ryantjesmith <rsmith@avans.nl>
2016-04-26 11:45:51 +0200

    partials seperated

--- app/js/controllers/dashboardCtrl.js
+++ app/js/controllers/dashboardCtrl.js
@@ -1,12 +1,4 @@
 module.exports = function ($scope, VisDataSet) {
-    $scope.slideDown = function(){
-        $scope.topBarStyle = {top: '100%'};
-        $scope.topContentStyle = {top: '0%'};
-    }
-    $scope.slideUp = function(){
-        $scope.topBarStyle = {top: '0%'};
-        $scope.topContentStyle = {top: '-100%'};
-    }
 
     $scope.data = {
         "nodes": VisDataSet([
