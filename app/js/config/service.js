b7c12b5058f394627b345d788474a74207523658
Niels Snakenborg <nc.snakenborg@student.avans.nl>
2016-04-28 18:02:03 +0200

    Auth Service added, Login added

--- app/js/config/service.js
+++ app/js/config/service.js
@@ -1,3 +1,4 @@
 module.exports = function (app) {
     app.factory('ProfileService', require('../services/profileService'));
+    app.factory('AuthService', require('../services/authService'));
 }
--- app/js/controllers/loginCtrl.js
+++ app/js/controllers/loginCtrl.js
@@ -1,3 +1,29 @@
-module.exports = function ($scope) {
+module.exports = function ($scope, AuthService) {
 
+    $scope.login = function(){
+        var email = $scope.email;
+        var password = $scope.password;
+        var validate = true;
+
+        if(password == "" || angular.isUndefined(password)){
+            $scope.message = "Voer een wachtwoord in in";
+            validate = false;
+        }
+
+        if(email == "" || angular.isUndefined(email)){
+            $scope.message = "Voer een email in";
+            validate = false;
+        }
+
+        if(validate){
+            AuthService.loginServer({ email: email, password: password}, {
+                onSuccess: function(result){
+                    // Go To dashboard
+                },
+                onError: function(err){
+                    $scope.message = err.data;
+                }
+            })
+        }
+    }
 };
