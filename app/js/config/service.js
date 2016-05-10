9c6ea2608d5350795ccf90cd20d7c9355ce2403b
tjvanos <tjos@student.avans.nl>
2016-05-10 12:18:42 +0200

    Keyword service added

--- app/js/config/service.js
+++ app/js/config/service.js
@@ -1,4 +1,5 @@
 module.exports = function (app) {
     app.factory('ProfileService', require('../services/profileService'));
+    app.factory('KeywordService', require('../services/keywordService'));
     app.factory('AuthService', require('../services/authService'));
 }
