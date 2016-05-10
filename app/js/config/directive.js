48ceb462fe66ea17a3c4c91ac3de47ae1124d71f
tjvanos <tjos@student.avans.nl>
2016-05-10 14:47:50 +0200

    Preview image
    image thumbnail

--- app/js/config/directive.js
+++ app/js/config/directive.js
@@ -1,19 +1,27 @@
 module.exports = function (app) {
 
-	var user_directive = function(){
-		return{
+	var user_directive = function () {
+		return {
 			restrict: "A",
 			templateUrl: "partials/directives/user_directive.html"
 		};
 	};
 
-	var school_directive = function(){
-		return{
+	var school_directive = function () {
+		return {
 			restrict: "A",
 			templateUrl: "partials/directives/school_directive.html"
 		};
 	};
 
+	var image_preview = function () {
+		return {
+			restrict: "A",
+			templateUrl: "partials/directives/previews/image_preview.html"
+		};
+	};
+
 	app.directive('userDirective', user_directive);
 	app.directive('schoolDirective', school_directive);
+	app.directive('imagePreview', image_preview);
 }
