4f1ce1333d05cfaf34efc11e5f2239be80aeb05b
tjvanos <tjos@student.avans.nl>
2016-05-17 14:35:58 +0200

    Added pdf, website and youtube preview

--- app/js/config/directive.js
+++ app/js/config/directive.js
@@ -28,8 +28,32 @@ module.exports = function (app) {
 		};
 	};
 
+	var pdf_preview = function () {
+		return {
+			restrict: "A",
+			templateUrl: "partials/directives/previews/pdf_preview.html"
+		};
+	};
+
+	var text_preview = function () {
+		return {
+			restrict: "A",
+			templateUrl: "partials/directives/previews/text_preview.html"
+		};
+	};
+	
+		var webpage_preview = function () {
+		return {
+			restrict: "A",
+			templateUrl: "partials/directives/previews/webpage_preview.html"
+		};
+	};
+
 	app.directive('userDirective', user_directive);
 	app.directive('schoolDirective', school_directive);
 	app.directive('imagePreview', image_preview);
 	app.directive('youtubePreview', youtube_preview);
+	app.directive('pdfPreview', pdf_preview);
+	app.directive('textPreview', text_preview);
+	app.directive('webpagePreview', webpage_preview);
 }
