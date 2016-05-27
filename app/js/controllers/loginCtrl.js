b7f6b3cc127e1a1b52ca7ebf02590b45883dfb86
ryantjesmith <rsmith@avans.nl>
2016-05-27 10:43:11 +0200

    styling changes

--- app/js/controllers/loginCtrl.js
+++ app/js/controllers/loginCtrl.js
@@ -1,33 +1,33 @@
-module.exports = function ($scope, AuthService) {
+module.exports = function ($scope, AuthService, $timeout) {
+    $timeout(function(){
+        $("#divLogin").addClass("show"); 
+    }, 3000);  
 
     $scope.login = function(){
         var email = $scope.email;
         var password = $scope.password;
-        var validate = true;
 
-        if(password == "" || angular.isUndefined(password)){
-            $scope.message = "Voer een wachtwoord in in";
-            validate = false;
-        }
+        AuthService.loginServer({ email: email, password: password}, {
+            onSuccess: function(result){
+              popupMessage("U bent succesvol ingelogd");
+              $("#divLogin").removeClass("show"); 
+              $("#divLogin").addClass("hide"); 
+              $('#divLoginBackgroundOverlay').hide();
+                // Go To dashboard
+            },
+            onError: function(err){
+                popupMessage(err.data);
+            }
+        })
+    }
 
-        if(email == "" || angular.isUndefined(email)){
-            $scope.message = "Voer een email in";
-            validate = false;
-        }
-
-        if(validate){
-            AuthService.loginServer({ email: email, password: password}, {
-                onSuccess: function(result){
-                  $scope.message = "";
-                  $('#divLogin').hide();
-                  $('#divLoginBackgroundOverlay').hide();
-                    // Go To dashboard
-                },
-                onError: function(err){
-                    $scope.message = err.data;
-                }
-            })
-        }
+    function popupMessage(message){
+        $scope.message = message;
+        $(".popup_message").addClass("flash_popup"); 
+        $timeout(function(){
+            $(".popup_message").removeClass("flash_popup"); 
+        }, 3000);  
+        
     }
 
 };
--- app/js/controllers/registerCtrl.js
+++ app/js/controllers/registerCtrl.js
@@ -1,4 +1,8 @@
-module.exports = function ($scope, AuthService, ResourcesService) {
+module.exports = function ($scope, AuthService, ResourcesService, $timeout) {
+
+    $timeout(function(){
+        $("#divRegister").addClass("show"); 
+    }, 3000);  
 
     $scope.imgLoadingPreviewHidden = true;
     $scope.file_changed = function (element) {
@@ -32,42 +36,11 @@ module.exports = function ($scope, AuthService, ResourcesService) {
         var image = $scope.image;
         var validate = true;
         
-        if (job == "" || angular.isUndefined(job)) {
-            $scope.message = "Voer een baan in";
-            validate = false;
-        }
-
-        if (lastName == "" || angular.isUndefined(lastName)) {
-            $scope.message = "Voer een achternaam in";
-            validate = false;
-        }
-
-        if (firstName == "" || angular.isUndefined(firstName)) {
-            $scope.message = "Voer een voornaam in";
-            validate = false;
-        }
-
         if (password2 != password) {
             $scope.message = "De wachtwoorden komen niet overeen";
             validate = false;
         }
 
-        if (password2 == "" || angular.isUndefined(password2)) {
-            $scope.message = "Voer een wachtwoord herhaling in";
-            validate = false;
-        }
-
-        if (password == "" || angular.isUndefined(password)) {
-            $scope.message = "Voer een wachtwoord in";
-            validate = false;
-        } else if (password.length < 8) {
-            $scope.message = "Wachtwoord te kort(minimaal 8 karakters)";
-        }
-
-        if (email == "" || angular.isUndefined(email)) {
-            $scope.message = "Voer een email in";
-            validate = false;
-        }
         if (validate) {
             AuthService.registerServer({
                 "firstName": firstName,
@@ -83,15 +56,26 @@ module.exports = function ($scope, AuthService, ResourcesService) {
                 "linkedIn": null
             }, {    
                     onSuccess: function (result) {
-                        $scope.message = "";
-                        $('#divRegister').hide();
+                        popupMessage("Succesvol geregistreerd");
+                        $("#divRegister").removeClass("show"); 
+                        $("#divRegister").addClass("hide"); 
                         $('#divLoginBackgroundOverlay').hide();
                         // Go To dashboard
                     },
                     onError: function (err) {
-                        $scope.message = err.data;
+                        popupMessage(err.data);
                     }
                 })
         }
     }
+
+    function popupMessage(message){
+        $scope.message = message;
+        $(".popup_message").addClass("flash_popup"); 
+        $timeout(function(){
+            $(".popup_message").removeClass("flash_popup"); 
+        }, 3000);  
+        
+    }
+
 };
