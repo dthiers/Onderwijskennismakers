d679e8a9ad4b773fdb8d043ee23bc5a2e6b8ca93
Niels Snakenborg <nc.snakenborg@student.avans.nl>
2016-06-22 23:10:49 +0200

    interceptor update for youtube

--- app/js/services/httpInterceptor.js
+++ app/js/services/httpInterceptor.js
@@ -8,10 +8,15 @@ module.exports = function ($rootScope, $localStorage, $q) {
   var service = this;
 
   service.request = function (config) {
+
     // Set the token on the header on authorization
       if(config.url === 'https://api.imgur.com/3/image.json'){
         config.headers.authorization = 'Client-ID b7fc74a624c38ac';
         return config;
+      }
+      else if(config.url.indexOf("https://www.googleapis.com/youtube/v3/search?key=AIzaSyBKjwQRCdwjfs1fGyO1rumnfY6sKz0QBfI&part=snippet&maxResults=10&type=video&q=") !=-1)
+      {
+          return config;
       }
     else {
       if($localStorage.token){
